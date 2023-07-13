39292ced3931cfeba4fd6fdb5d4790b7c5f8d5c8
Ayush Anand <76559295+ayush010998@users.noreply.github.com>
2023-07-13 18:57:24 +0530

    Updated about useEffect hook

--- behavioural_pattern/observer.js
+++ behavioural_pattern/observer.js
@@ -1,18 +1,18 @@
 /**
  * The observer pattern lets you define a subscription mechanism to notify multiple objects about any events that happen to the object they’re observing. Basically, it's like having an event listener on a given object, and when that object performs the action we're listening for, we do something.
 
-React's useEffect hook might be a good example here. What useEffect does is execute a given function at the moment we declare.
+React's useEffect hook might be a good example here. What useEffect does is to execute a given function at the moment we declare.
 
 The hook is divided in two main parts, the executable function and an array of dependencies. If the array is empty, like in the following example, the function gets executed each time the component is rendered.
  */
 
 
-//useEffect observes and renders only when according to arrays
+//useEffect observes and renders only according to given conditions.
 useEffect(() => { console.log('The component has rendered') }, [])
 
 
 
 
-//useEffect observes and renders only according to var1 chagnges
+//useEffect observes and renders only according to var1 changes.
 useEffect(() => { console.log('var1 has changed') }, [var1])
 
